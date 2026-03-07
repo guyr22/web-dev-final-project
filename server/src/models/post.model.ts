90ff2b41286ae66c58800a6274e13aee11350537
Guy Rozenbaum <guyr2121@gmail.com>
2026-03-07 23:14:23 +0200

    add comments section and profile image for posts

--- server/src/models/post.model.ts
+++ server/src/models/post.model.ts
@@ -29,7 +29,8 @@ const postSchema = new Schema<IPost>({
     },
     comments: [{
         userId: {
-            type: String,
+            type: Schema.Types.ObjectId,
+            ref: 'User',
             required: true
         },
         content: {
